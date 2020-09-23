06c7033036d828ee5341fa80c2e6068b0e828fe8
Ivan Terekhin <i.terhin@gmail.com>
2020-09-23 13:19:48 +0300

    custom editing completed

--- lib/src/annotation_editing_controller.dart
+++ lib/src/annotation_editing_controller.dart
@@ -3,8 +3,8 @@ part of flutter_mentions;
 /// A custom implementation of [TextEditingController] to support @ mention or other
 /// trigger based mentions.
 class AnnotationEditingController extends TextEditingController {
-  Map<String, Annotation> _mapping;
-  String _pattern;
+  final Map<String, Annotation> _mapping;
+  final String _pattern;
 
   // Generate the Regex pattern for matching all the suggestions in one.
   AnnotationEditingController(this._mapping)
@@ -24,10 +24,7 @@ class AnnotationEditingController extends TextEditingController {
 
         // Default markup format for mentions
         if (!mention.disableMarkup) {
-          return mention.markupBuilder != null
-              ? mention.markupBuilder(
-                  mention.trigger, mention.id, mention.display)
-              : '${mention.trigger}[__${mention.id}__](__${mention.display}__)';
+          return '<span data-entity="comment">${mention.display}</span>';
         } else {
           return match[0];
         }
@@ -38,16 +35,6 @@ class AnnotationEditingController extends TextEditingController {
     );
 
     return someVal;
-  }
-
-  Map<String, Annotation> get mapping {
-    return _mapping;
-  }
-
-  set mapping(Map<String, Annotation> _mapping) {
-    this._mapping = _mapping;
-
-    _pattern = "(${_mapping.keys.map((key) => key).join('|')})";
   }
 
   @override
@@ -80,7 +67,6 @@ class AnnotationEditingController extends TextEditingController {
         return '';
       },
     );
-
     return TextSpan(style: style, children: children);
   }
 }
