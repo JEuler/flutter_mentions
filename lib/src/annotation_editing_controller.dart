1a8dca9e3ffa2a51c3956ecfd5d04ed284acf28c
Ivan Terekhin <i.terhin@gmail.com>
2020-09-29 13:23:39 +0300

    Update annotation_editing_controller.dart

--- lib/src/annotation_editing_controller.dart
+++ lib/src/annotation_editing_controller.dart
@@ -3,8 +3,8 @@ part of flutter_mentions;
 /// A custom implementation of [TextEditingController] to support @ mention or other
 /// trigger based mentions.
 class AnnotationEditingController extends TextEditingController {
-  final Map<String, Annotation> _mapping;
-  final String _pattern;
+  Map<String, Annotation> _mapping;
+  String _pattern;
 
   // Generate the Regex pattern for matching all the suggestions in one.
   AnnotationEditingController(this._mapping)
@@ -37,6 +37,16 @@ class AnnotationEditingController extends TextEditingController {
     return someVal;
   }
 
+  Map<String, Annotation> get mapping {
+    return _mapping;
+  }
+
+  set mapping(Map<String, Annotation> _mapping) {
+    this._mapping = _mapping;
+
+    _pattern = "(${_mapping.keys.map((key) => key).join('|')})";
+  }
+
   @override
   TextSpan buildTextSpan({TextStyle style, bool withComposing}) {
     var children = <InlineSpan>[];
@@ -67,6 +77,7 @@ class AnnotationEditingController extends TextEditingController {
         return '';
       },
     );
+
     return TextSpan(style: style, children: children);
   }
 }
